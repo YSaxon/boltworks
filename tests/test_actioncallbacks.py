fee72dafae68c8979bc3b175328ed1fa43aad9dd
Yaakov Saxon <ysaxon@nabancard.com>
2023-03-06 12:51:57 -0500

    add tests for actioncallbacks and nodetree

---
+++ tests/test_actioncallbacks.py
@@ -0,0 +1,95 @@
+from datetime import datetime, timedelta
+from functools import partial
+import json
+from typing import Tuple
+from unittest.mock import Mock
+from diskcache import Cache
+import pytest
+from slack_bolt import App, Args
+from boltworks import ActionCallbacks,DiskCacheKVStore
+import dill
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+import sys
+import os
+from slack_sdk.models.blocks import SectionBlock
+from slack_sdk.web import SlackResponse
+
+from slack_bolt import Respond,Say
+
+from tests.common import get_blocks_from_response_with_assertions, mock_an_args
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../..")
+
+from boltworks import ActionCallbacks
+from boltworks import DiskCacheKVStore
+
+from common import TOKEN,APPTOKEN,TEST_CHANNEL,DISK_CACHE_DIR
+
+
+app = App(token=TOKEN)
+
+
+
+@pytest.fixture
+def fixture():  
+    app = App(token=TOKEN)
+    handler=SocketModeHandler(app,app_token=APPTOKEN)
+    handler.connect()
+    
+    disk_cache=DiskCacheKVStore(Cache(directory=DISK_CACHE_DIR))
+    callbacks=ActionCallbacks(app,disk_cache.using_serializer(dill))
+    
+    yield app,callbacks
+    
+    disk_cache._diskcache.close()
+    handler.disconnect()
+
+
+def test_button_posted_same_as_expected(fixture:Tuple[App, ActionCallbacks]):
+    app,callbacks=fixture
+    def callback_func(args:Args):
+        args.respond("C")
+    button=callbacks.get_button_register_callback("(button)",callback_func)
+    block=SectionBlock(text="Click here",accessory=button)
+    response=app.client.chat_postMessage(blocks=[block],channel=TEST_CHANNEL)
+    response_blocks=get_blocks_from_response_with_assertions(response)
+    response_button=response_blocks[0]['accessory']
+    assert(response_button==button.to_dict())
+
+def test_simple(fixture:Tuple[App, ActionCallbacks]):
+    app,callbacks=fixture
+    def callback_func(args:Args):
+        args.respond("C")
+    button=callbacks.get_button_register_callback("(button)",callback_func)
+    button=button.to_dict() #assuming test_button_posted_same_as_expected succeeds, this is the same as posting it
+        
+    args_mock,respond_mock=mock_an_args()
+    args_mock.action=dict(action_id=button['action_id'])
+    callbacks.do_callback_action(args=args_mock)
+    respond_mock.assert_called_once_with("C")
+    
+def test_closure(fixture:Tuple[App, ActionCallbacks]):
+    app,callbacks=fixture
+    closure_var="A"
+    def callback_func(args:Args):
+        args.respond(closure_var)
+    button=callbacks.get_button_register_callback("(button)",callback_func)
+    button=button.to_dict() #assuming test_button_posted_same_as_expected succeeds, this is the same as posting it
+    
+    args_mock,respond_mock=mock_an_args()
+    args_mock.action=dict(action_id=button['action_id'])
+    callbacks.do_callback_action(args=args_mock)
+    respond_mock.assert_called_once_with("A")
+    
+    
+def test_partial_with_injected_var(fixture:Tuple[App, ActionCallbacks]):
+    app,callbacks=fixture
+    def callback_func(args:Args,other_param):
+        args.respond(other_param)
+    button=callbacks.get_button_register_callback("(button)",partial(callback_func,other_param="B"))
+    button=button.to_dict() #assuming test_button_posted_same_as_expected succeeds, this is the same as posting it
+          
+    args_mock,respond_mock=mock_an_args()
+    args_mock.action=dict(action_id=button['action_id'])
+    callbacks.do_callback_action(args=args_mock)
+    respond_mock.assert_called_once_with("B")
---
+++ tests/test_nodetreeui.py
@@ -0,0 +1,317 @@
+
+
+import copy
+import pytest
+from slack_bolt import App
+from ..boltworks import *
+from typing import Tuple
+from unittest.mock import Mock
+from diskcache import Cache
+import dill
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+from .common import TOKEN,APPTOKEN, TEST_CHANNEL, assert_block_text_equals, fake_a_respond_from_response, get_blocks_from_response_with_assertions
+
+
+
+app = App(token=TOKEN)
+
+DISK_CACHE_DIR="/tmp/diskcache"
+
+
+@pytest.fixture
+def fixture():  
+    app = App(token=TOKEN)
+    handler=SocketModeHandler(app,app_token=APPTOKEN)
+    handler.connect()
+    
+    disk_cache=DiskCacheKVStore(Cache(directory=DISK_CACHE_DIR)).using_serializer(dill)
+    treeui=TreeNodeUI(app,disk_cache)
+    
+    yield app,treeui
+    
+    handler.disconnect()
+    
+    
+def test_simple_actual_post(fixture:Tuple[App,TreeNodeUI]):
+    app,treeui=fixture
+    rootnode=TreeNode.withSimpleSideButton("parent",[TreeNode("child1"), TreeNode("child2")])
+    response=treeui.post_single_node(TEST_CHANNEL,rootnode)
+    blocks=get_blocks_from_response_with_assertions(response)
+    assert len(blocks)==1
+    assert_block_text_equals(blocks[0],"parent")
+    assert 'accessory' in blocks[0]
+    
+    
+def test_simple_actualpost_mockexpand(fixture:Tuple[App,TreeNodeUI]):
+    app,treeui=fixture
+    rootnode=TreeNode.withSimpleSideButton("parent",[TreeNode("child1"), TreeNode("child2")])
+    response=treeui.post_single_node(TEST_CHANNEL,rootnode)
+    blocks=get_blocks_from_response_with_assertions(response)
+    button=blocks[0]['accessory']
+    
+    ack=Mock()
+    respond=Mock()
+    treeui._do_callback_action(action=button,ack=ack,respond=respond)
+    
+    ack.assert_called_once()
+    respond.assert_called_once()
+    
+    print(respond.call_args)
+    assert respond.call_args.kwargs['replace_original']
+    replace_blocks=respond.call_args.kwargs['blocks']
+    
+    assert len(replace_blocks)==3
+    assert_block_text_equals(replace_blocks[0],'parent')
+    assert_block_text_equals(replace_blocks[1],'child1')
+    assert_block_text_equals(replace_blocks[2],'child2')
+
+    
+def test_simple_actualpost_actualexpand(fixture:Tuple[App,TreeNodeUI]):
+    app,treeui=fixture
+    rootnode=TreeNode.withSimpleSideButton("parent",[TreeNode("child1"), TreeNode("child2")])
+    response=treeui.post_single_node(TEST_CHANNEL,rootnode)
+    blocks=get_blocks_from_response_with_assertions(response)
+    button=blocks[0]['accessory']
+    
+    ack=Mock()
+    respond=fake_a_respond_from_response(response)
+    callback_response=treeui._do_callback_action(action=button,ack=ack,respond=respond)
+    
+    callback_blocks=get_blocks_from_response_with_assertions(callback_response)
+    
+    assert len(callback_blocks)==3
+    assert_block_text_equals(callback_blocks[0],'parent')
+    assert_block_text_equals(callback_blocks[1],'child1')
+    assert_block_text_equals(callback_blocks[2],'child2')
+    
+    
+def test_complex_actual_multibutton(fixture:Tuple[App,TreeNodeUI]):
+    app,treeui=fixture
+    
+    
+    ANodes=[TreeNode(f"A{i}") for i in range(13)]
+    BNodes=[TreeNode(f"B{i}") for i in range(10)]
+    CNodes=[TreeNode(f"C{i}") for i in range(9)]
+    CNode3Children=[TreeNode(f"C3_{i}") for i in range(3)]
+    CNodes[3].children_containers=[ButtonChildContainer(CNode3Children)]
+    CNodes[3].first_child_container_on_side=True
+    
+    rootnode=TreeNode(
+        "parent",
+       children_containers=[
+           ButtonChildContainer(ANodes,static_button_text="Alpha",child_pageination=5),
+           ButtonChildContainer(BNodes,static_button_text="Bravo",child_pageination=5),
+           ButtonChildContainer(CNodes,static_button_text="Charlie",child_pageination=10),
+       ]
+    )
+                      
+    response=treeui.post_single_node(TEST_CHANNEL,rootnode)
+    blocks=get_blocks_from_response_with_assertions(response)
+    
+    assert len(blocks)==2 #parent + actions
+    button_b=blocks[1]['elements'][1]
+    assert_block_text_equals(button_b,"Bravo")
+    
+    ack=Mock()
+    respond=fake_a_respond_from_response(response)
+    
+    #expand bravo
+    callback_response=treeui._do_callback_action(action=button_b,ack=ack,respond=respond)
+    callback_blocks=get_blocks_from_response_with_assertions(callback_response)
+    assert len(callback_blocks)==8 #parent+actions+5 children+navigation
+    assert_block_text_equals(callback_blocks[0],'parent')
+    assert_block_text_equals(callback_blocks[2],'B0')
+    assert_block_text_equals(callback_blocks[-2],'B4')
+    
+    #expand alpha
+    button_a=callback_blocks[1]['elements'][0]
+    callback_response=treeui._do_callback_action(action=button_a,ack=ack,respond=respond)
+    callback_blocks=get_blocks_from_response_with_assertions(callback_response)
+    assert len(callback_blocks)==8 #parent+actions+5 children+navigation
+    assert_block_text_equals(callback_blocks[0],'parent')
+    assert_block_text_equals(callback_blocks[2],'A0')
+    
+    #page forward alpha
+    button_forward=callback_blocks[-1]['elements'][-1]
+    callback_response=treeui._do_callback_action(action=button_forward,ack=ack,respond=respond)
+    callback_blocks=get_blocks_from_response_with_assertions(callback_response)
+    assert len(callback_blocks)==8 #parent+actions+5 children+navigation
+    assert_block_text_equals(callback_blocks[0],'parent')
+    assert_block_text_equals(callback_blocks[2],'A5')
+    
+    #page forward alpha
+    button_forward=callback_blocks[-1]['elements'][-1]
+    callback_response=treeui._do_callback_action(action=button_forward,ack=ack,respond=respond)
+    callback_blocks=get_blocks_from_response_with_assertions(callback_response)
+    assert len(callback_blocks)==6 #parent + actions + 3 children + navigation
+    assert_block_text_equals(callback_blocks[0],'parent')
+    assert_block_text_equals(callback_blocks[2],'A10')
+    
+    #page backwards alpha
+    button_forward=callback_blocks[-1]['elements'][0]
+    callback_response=treeui._do_callback_action(action=button_forward,ack=ack,respond=respond)
+    callback_blocks=get_blocks_from_response_with_assertions(callback_response)
+    assert len(callback_blocks)==8 #parent+actions+5 children+navigation
+    assert_block_text_equals(callback_blocks[0],'parent')
+    assert_block_text_equals(callback_blocks[2],'A5')
+    
+    #contract alpha
+    button_a=callback_blocks[1]['elements'][0]
+    callback_response=treeui._do_callback_action(action=button_a,ack=ack,respond=respond)
+    callback_blocks=get_blocks_from_response_with_assertions(callback_response)
+    assert len(callback_blocks)==2 #parent + actions
+    
+    #expand charlie
+    button_c=callback_blocks[1]['elements'][2]
+    callback_response=treeui._do_callback_action(action=button_c,ack=ack,respond=respond)
+    callback_blocks=get_blocks_from_response_with_assertions(callback_response)
+    assert len(callback_blocks)==11 #parent + actions + 9 children
+    
+    #expand c3
+    c3=callback_blocks[2+3]
+    c3_button=c3['accessory']
+    callback_response=treeui._do_callback_action(action=c3_button,ack=ack,respond=respond)
+    callback_blocks=get_blocks_from_response_with_assertions(callback_response)
+    assert len(callback_blocks)==11+3 #parent + actions + 9 children + 3 subchildren
+    
+    
+    
+def test_complex_actual_overflowmenu(fixture:Tuple[App,TreeNodeUI]):
+    app,treeui=fixture
+    
+    
+    AOption=MenuOption("Alpha",[TreeNode(f"A{i}") for i in range(13)])
+    BOption=MenuOption("Bravo",[TreeNode(f"B{i}") for i in range(10)])
+    COption=MenuOption("Charlie",[TreeNode(f"C{i}") for i in range(5)])
+    # CNode3Children=[TreeNode(f"C3_{i}") for i in range(3)]
+    # COption.nodes[3].children_containers.append(ButtonChildContainer(CNode3Children))
+    # COption.nodes[3].first_child_container_on_side=True
+    
+    rootnode=TreeNode(
+        "parent",
+       children_containers=[   
+           OverflowMenuChildContainer([AOption,BOption,COption],child_pageination=5),
+       ],
+       first_child_container_on_side=False
+    )
+                      
+    response=treeui.post_single_node(TEST_CHANNEL,rootnode)
+    blocks=get_blocks_from_response_with_assertions(response)
+    
+    assert len(blocks)==2 #parent + actions
+    menu=blocks[1]['elements'][0]
+    assert_block_text_equals(menu['options'][1],"Bravo")
+    
+    ack=Mock()
+    respond=fake_a_respond_from_response(response)
+    
+    #expand bravo
+    action_for_optionb=dict(action_id=menu['action_id'],selected_option=menu['options'][1])
+    callback_response=treeui._do_callback_action(action=action_for_optionb,ack=ack,respond=respond)
+    callback_blocks=get_blocks_from_response_with_assertions(callback_response)
+    assert len(callback_blocks)==8 #parent+actions+5 children+navigation
+    assert_block_text_equals(callback_blocks[0],'parent')
+    assert_block_text_equals(callback_blocks[2],'B0')
+    assert_block_text_equals(callback_blocks[-2],'B4')
+
+def test_complex_actual_menuoption(fixture:Tuple[App,TreeNodeUI]):
+    app,treeui=fixture
+    
+    
+    AOption=MenuOption("Alpha",[TreeNode(f"A{i}") for i in range(13)])
+    BOption=MenuOption("Bravo",[TreeNode(f"B{i}") for i in range(10)])
+    COption=MenuOption("Charlie",[TreeNode(f"C{i}") for i in range(9)])
+    CNode3Children=[TreeNode(f"C3_{i}") for i in range(3)]
+    COption.nodes[3].children_containers.append(ButtonChildContainer(CNode3Children))
+    COption.nodes[3].first_child_container_on_side=True
+    
+    rootnode=TreeNode(
+        "parent",
+       children_containers=[   
+           StaticSelectMenuChildContainer([AOption,BOption,COption],child_pageination=5),
+       ],
+       first_child_container_on_side=False
+    )
+                      
+    response=treeui.post_single_node(TEST_CHANNEL,rootnode)
+    blocks=get_blocks_from_response_with_assertions(response)
+    
+    assert len(blocks)==2 #parent + actions
+    menu=blocks[1]['elements'][0]
+    assert_block_text_equals(menu['options'][1],"Bravo")
+    
+    ack=Mock()
+    respond=fake_a_respond_from_response(response)
+    
+    #expand bravo
+    action_for_optionb=dict(action_id=menu['action_id'],selected_option=menu['options'][1])
+    callback_response=treeui._do_callback_action(action=action_for_optionb,ack=ack,respond=respond)
+    callback_blocks=get_blocks_from_response_with_assertions(callback_response)
+    assert len(callback_blocks)==8 #parent+actions+5 children+navigation
+    assert_block_text_equals(callback_blocks[0],'parent')
+    assert_block_text_equals(callback_blocks[2],'B0')
+    assert_block_text_equals(callback_blocks[-2],'B4')
+    
+    #expand alpha
+    menu=callback_blocks[1]['elements'][0]
+    action_for_optiona=dict(action_id=menu['action_id'],selected_option=menu['options'][1+0]) #need to add one for the deselect option on top
+    callback_response=treeui._do_callback_action(action=action_for_optiona,ack=ack,respond=respond)
+    callback_blocks=get_blocks_from_response_with_assertions(callback_response)
+    assert len(callback_blocks)==8 #parent+actions+5 children+navigation
+    assert_block_text_equals(callback_blocks[0],'parent')
+    assert_block_text_equals(callback_blocks[2],'A0')
+    
+    #page forward alpha
+    button_forward=callback_blocks[-1]['elements'][-1]
+    callback_response=treeui._do_callback_action(action=button_forward,ack=ack,respond=respond)
+    callback_blocks=get_blocks_from_response_with_assertions(callback_response)
+    assert len(callback_blocks)==8 #parent+actions+5 children+navigation
+    assert_block_text_equals(callback_blocks[0],'parent')
+    assert_block_text_equals(callback_blocks[2],'A5')
+    
+    # #page forward alpha
+    # button_forward=callback_blocks[-1]['elements'][-1]
+    # callback_response=treeui._do_callback_action(action=button_forward,ack=ack,respond=respond)
+    # callback_blocks=get_blocks_from_response_with_assertions(callback_response)
+    # assert len(callback_blocks)==6 #parent + actions + 3 children + navigation
+    # assert_block_text_equals(callback_blocks[0],'parent')
+    # assert_block_text_equals(callback_blocks[2],'A10')
+    
+    # #page backwards alpha
+    # button_forward=callback_blocks[-1]['elements'][0]
+    # callback_response=treeui._do_callback_action(action=button_forward,ack=ack,respond=respond)
+    # callback_blocks=get_blocks_from_response_with_assertions(callback_response)
+    # assert len(callback_blocks)==8 #parent+actions+5 children+navigation
+    # assert_block_text_equals(callback_blocks[0],'parent')
+    # assert_block_text_equals(callback_blocks[2],'A5')
+    
+    #contract all
+    menu=callback_blocks[1]['elements'][0]
+    action_for_deselect=dict(action_id=menu['action_id'],selected_option=menu['options'][0])
+    callback_response=treeui._do_callback_action(action=action_for_deselect,ack=ack,respond=respond)
+    callback_blocks=get_blocks_from_response_with_assertions(callback_response)
+    assert len(callback_blocks)==2 #parent + actions
+    
+    #expand charlie
+    menu=callback_blocks[1]['elements'][0]
+    action_charlie=dict(action_id=menu['action_id'],selected_option=menu['options'][-1])
+    callback_response=treeui._do_callback_action(action=action_charlie,ack=ack,respond=respond)
+    callback_blocks=get_blocks_from_response_with_assertions(callback_response)
+    assert len(callback_blocks)==8 #parent+actions+5 children+navigation
+    assert_block_text_equals(callback_blocks[0],'parent')
+    assert_block_text_equals(callback_blocks[2],'C0')
+    
+    #expand c3
+    c3=callback_blocks[2+3]
+    c3_button=c3['accessory']
+    callback_response=treeui._do_callback_action(action=c3_button,ack=ack,respond=respond)
+    callback_blocks=get_blocks_from_response_with_assertions(callback_response)
+    assert len(callback_blocks)==8+3 #parent+actions+5 children+navigation + 3 grandchildren
+    
+    
+    
+
+    
+    
+    
