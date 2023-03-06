32769a7fdbc49a8aab342995843a9e22507b32ea
Yaakov Saxon <ysaxon@nabancard.com>
2023-03-06 17:21:29 -0500

    improve threadcallbacks

--- boltworks/callbacks/thread_callbacks.py
+++ boltworks/callbacks/thread_callbacks.py
@@ -1,21 +1,22 @@
 import re
-from typing import Any, Callable
+from typing import Any, Callable, Protocol
 
 from slack_bolt import App, Args
 from ..helper.kvstore import KVStoreWithSerializer
 
-
+class ThreadCallbackFunction(Protocol):
+     def __call__(self, args:Args): ...
 class MsgThreadCallbacks():
     def __init__(self,app:App,kvstore:KVStoreWithSerializer):
         self._callback_store=kvstore.namespaced("thread_callback")
         app.message(re.compile('.*'))(self.check_for_thread_reply_callback)
 
-    def register_thread_reply_callback(self, ts:str, callback:Callable[[Args],Any]):
+    def register_thread_reply_callback(self, ts:str, callback:ThreadCallbackFunction):
         self._callback_store[ts]=callback
 
     def check_for_thread_reply_callback(self,args:Args):
         if 'thread_ts' in args.payload:
             thread_ts=args.payload['thread_ts']
             if thread_ts in self._callback_store:
-                callback=self._callback_store[thread_ts]
+                callback:ThreadCallbackFunction=self._callback_store[thread_ts] # type: ignore  
                 callback(args)
